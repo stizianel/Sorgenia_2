3aa08dbe5e6f32e31023ab9fa816c37be2cdfecb
stizianel <stizianel@gmail.com>
2013-06-04 13:00:04 +0200

    al 4 6 pranzo

--- src/A_document_130.py
+++ src/A_document_130.py
@@ -6,4 +6,4 @@ Created on 10/ott/2012
 from it_eutile_utils_aggregator.AggregatorManager import AggregatorManagerExMny
 
 
-AggregatorManagerExMny('document_aggregator',100,'DocumentAggregator', filename='document.xml')
+AggregatorManagerExMny('document_aggregator',10000,'DocumentAggregator', filename='document.xml')
